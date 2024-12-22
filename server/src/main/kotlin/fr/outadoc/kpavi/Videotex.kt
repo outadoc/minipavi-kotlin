4dd37580a63b96dac24abdf0ac29bd08ff6cabe5
Baptiste Candellier <baptiste@candellier.me>
2024-12-23 00:01:01 +0100

    Reuse fr comments

--- server/src/main/kotlin/fr/outadoc/kpavi/Videotex.kt
+++ server/src/main/kotlin/fr/outadoc/kpavi/Videotex.kt
@@ -3,242 +3,242 @@ package fr.outadoc.kpavi
 object Videotex {
 
     /**
-     * Move cursor left
+     * Déplacement curseur vers la gauche.
      */
     const val VDT_LEFT = "\u0008"
 
     /**
-     * Move cursor right
+     * Déplacement curseur vers la droite.
      */
     const val VDT_RIGHT = "\u0009"
 
     /**
-     * Move cursor down
+     * Déplacement curseur vers le bas.
      */
     const val VDT_DOWN = "\u000A"
 
     /**
-     * Move cursor up
+     * Déplacement curseur vers le haut.
      */
     const val VDT_UP = "\u000B"
 
     /**
-     * Carriage return (start of line)
+     * Retour charriot (début de ligne).
      */
     const val VDT_CR = "\u000D"
 
     /**
-     * Carriage return + move down
+     * Retour charriot + déplacement vers le bas.
      */
     const val VDT_CRLF = "\u000D\u000A"
 
     /**
-     * Clear screen
+     * Effacement écran.
      */
     const val VDT_CLR = "\u000C"
 
     /**
-     * Character set G0
+     * Jeu de caractères G0.
      */
     const val VDT_G0 = "\u000F"
 
     /**
-     * Character set G1
+     * Jeu de caractères G1.
      */
     const val VDT_G1 = "\u000E"
 
     /**
-     * Character set G2
+     * Jeu de caractères G2.
      */
     const val VDT_G2 = "\u0019"
 
     /**
-     * Cursor positioning
+     * Positionnement du curseur.
      */
     const val VDT_POS = "\u001F"
 
     /**
-     * Character repetition
+     * Répétition caractère.
      */
     const val VDT_REP = "\u0012"
 
     /**
-     * Cursor on
+     * Curseur allumé.
      */
     const val VDT_CURON = "\u0011"
 
     /**
-     * Cursor off
+     * Curseur éteint.
      */
     const val VDT_CUROFF = "\u0014"
 
     /**
-     * Clear end of line
+     * Effacement fin de ligne.
      */
     const val VDT_CLRLN = "\u0018"
 
     /**
-     * Normal character size
+     * Taille des caratcères normale.
      */
     const val VDT_SZNORM = "\u001B\u004C"
 
     /**
-     * Double height character size
+     * Taille des caratcères double hauteur.
      */
     const val VDT_SZDBLH = "\u001B\u004D"
 
     /**
-     * Double width character size
+     * Taille des caratcères double largeur.
      */
     const val VDT_SZDBLW = "\u001B\u004E"
 
     /**
-     * Double height + double width character size
+     * Taille des caractères double hauteur + double largeur.
      */
     const val VDT_SZDBLHW = "\u001B\u004F"
 
     /**
-     * Text color black
+     * Couleur texte noir.
      */
     const val VDT_TXTBLACK = "\u001B@"
 
     /**
-     * Text color red
+     * Couleur texte rouge.
      */
     const val VDT_TXTRED = "\u001BA"
 
     /**
-     * Text color green
+     * Couleur texte vert.
      */
     const val VDT_TXTGREEN = "\u001BB"
 
     /**
-     * Text color yellow
+     * Couleur texte jaune.
      */
     const val VDT_TXTYELLOW = "\u001BC"
 
     /**
-     * Text color blue
+     * Couleur texte bleu.
      */
     const val VDT_TXTBLUE = "\u001BD"
 
     /**
-     * Text color magenta
+     * Couleur texte magenta.
      */
     const val VDT_TXTMAGENTA = "\u001BE"
 
     /**
-     * Text color cyan
+     * Couleur texte cyan.
      */
     const val VDT_TXTCYAN = "\u001BF"
 
     /**
-     * Text color white
+     * Couleur texte blanc.
      */
     const val VDT_TXTWHITE = "\u001BG"
 
     /**
-     * Background color black
+     * Couleur fond noir.
      */
     const val VDT_BGBLACK = "\u001BP"
 
     /**
-     * Background color red
+     * Couleur fond rouge.
      */
     const val VDT_BGRED = "\u001BQ"
 
     /**
-     * Background color green
+     * Couleur fond vert.
      */
     const val VDT_BGGREEN = "\u001BR"
 
     /**
-     * Background color yellow
+     * Couleur fond jaune.
      */
     const val VDT_BGYELLOW = "\u001BS"
 
     /**
-     * Background color blue
+     * Couleur fond bleu.
      */
     const val VDT_BGBLUE = "\u001BT"
 
     /**
-     * Background color magenta
+     * Couleur fond magenta.
      */
     const val VDT_BGMAGENTA = "\u001BU"
 
     /**
-     * Background color cyan
+     * Couleur fond cyan.
      */
     const val VDT_BGCYAN = "\u001BV"
 
     /**
-     * Background color white
+     * Couleur fond blanc.
      */
     const val VDT_BGWHITE = "\u001BW"
 
     /**
-     * Blinking text
+     * Clignotement.
      */
     const val VDT_BLINK = "\u001BH"
 
     /**
-     * Stop blinking
+     * Arrêt clignotement.
      */
     const val VDT_FIXED = "\u001BI"
 
     /**
-     * Stop underline
+     * Arrêt soulignement.
      */
     const val VDT_STOPUNDERLINE = "\u001BY"
 
     /**
-     * Start underline
+     * Début soulignement.
      */
     const val VDT_STARTUNDERLINE = "\u001BZ"
 
     /**
-     * Normal background
+     * Fond normal.
      */
     const val VDT_FDNORM = "\u001B\\"
 
     /**
-     * Inverted background
+     * Fond inversé.
      */
     const val VDT_FDINV = "\u001B]"
 
     /**
-     * Switch keyboard to lowercase
+     * Passage clavier en minuscules.
      */
     const val PRO_MIN = "\u001B\u003A\u0069\u0045"
 
     /**
-     * Switch keyboard to uppercase
+     * Passage clavier en majuscules.
      */
     const val PRO_MAJ = "\u001B\u003A\u006A\u0045"
 
     /**
-     * Turn off local echo
+     * Arrêt echo local.
      */
     const val PRO_LOCALECHO_OFF = "\u001B\u003B\u0060\u0058\u0051"
 
     /**
-     * Turn on local echo
+     * Marche echo local.
      */
     const val PRO_LOCALECHO_ON = "\u001B\u003B\u0061\u0058\u0051"
 
     /**
-     * Enable roller mode
+     * Mode rouleau actif.
      */
     const val PRO_ROULEAU_ON = "\u001B\u003A\u0069\u0043"
 
     /**
-     * Disable roller mode
+     * Mode rouleau inactif.
      */
     const val PRO_ROULEAU_OFF = "\u001B\u003A\u006A\u0043"
 
     /**
-     * Reset character sets
+     * "Réinitialisation" des jeux de caractères normaux.
      */
     const val VDT_RESET_DRCS = "\u001B\u0028\u0040\u001B\u0029\u0063"
 
@@ -413,7 +413,7 @@ object Videotex {
      */
     fun String.toG2(): String {
         return replace(
-            regex = Regex("[\\x00-\\x1F\\x81\\x8D\\x8F\\x90\\x9D]"),
+            regex = Regex("[\\u0000-\\u001F\\u0081\\u008D\\u008F\\u0090\\u009D]"),
             replacement = " "
         )
             .map { char ->
