f1db07c186be3e2af62c289b8a17202c9aa4620f
Baptiste Candellier <baptiste@candellier.me>
2024-12-25 18:18:47 +0100

    Fix DTO import issues

--- minipavi/src/commonMain/kotlin/fr/outadoc/pavikt/minipavi/internal/data/serializer/FunctionKeySetSerializer.kt
+++ minipavi/src/commonMain/kotlin/fr/outadoc/pavikt/minipavi/internal/data/serializer/FunctionKeySetSerializer.kt
@@ -1,6 +1,6 @@
 package fr.outadoc.pavikt.minipavi.internal.data.serializer
 
-import fr.outadoc.pavikt.minipavi.internal.data.model.CommandDTO
+import fr.outadoc.pavikt.minipavi.internal.data.model.ServiceResponseDTO
 import kotlinx.serialization.KSerializer
 import kotlinx.serialization.descriptors.PrimitiveKind
 import kotlinx.serialization.descriptors.PrimitiveSerialDescriptor
@@ -8,21 +8,21 @@ import kotlinx.serialization.descriptors.SerialDescriptor
 import kotlinx.serialization.encoding.Decoder
 import kotlinx.serialization.encoding.Encoder
 
-internal object FunctionKeySetSerializer : KSerializer<Set<CommandDTO.FunctionKey>> {
+internal object FunctionKeySetSerializer : KSerializer<Set<ServiceResponseDTO.Command.FunctionKey>> {
 
     override val descriptor: SerialDescriptor =
         PrimitiveSerialDescriptor("FunctionKeySetSerializer", PrimitiveKind.INT)
 
-    override fun deserialize(decoder: Decoder): Set<CommandDTO.FunctionKey> {
+    override fun deserialize(decoder: Decoder): Set<ServiceResponseDTO.Command.FunctionKey> {
         val input = decoder.decodeInt()
-        return CommandDTO.FunctionKey.entries
+        return ServiceResponseDTO.Command.FunctionKey.entries
             .filter { functionKey ->
                 input and functionKey.value != 0
             }
             .toSet()
     }
 
-    override fun serialize(encoder: Encoder, value: Set<CommandDTO.FunctionKey>) {
+    override fun serialize(encoder: Encoder, value: Set<ServiceResponseDTO.Command.FunctionKey>) {
         encoder.encodeInt(
             value.fold(0) { acc, functionKey ->
                 acc or functionKey.value
