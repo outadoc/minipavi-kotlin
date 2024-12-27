6be525ecbc27e133d65bd0b36159ab875906eeac
Baptiste Candellier <baptiste@candellier.me>
2024-12-27 17:26:41 +0100

    Add maven-publish plugin

--- core/build.gradle.kts
+++ core/build.gradle.kts
@@ -8,6 +8,7 @@ plugins {
     alias(libs.plugins.dokka)
     alias(libs.plugins.kotlin.binaryCompatibilityValidator)
     alias(libs.plugins.spotless)
+    `maven-publish`
 }
 
 group = "fr.outadoc.minipavi"
@@ -62,5 +63,42 @@ tasks.withType<DokkaTaskPartial>().configureEach {
                 url.set(URL("https://kotlinlang.org/api/kotlinx-io/"))
             }
         }
+    }
+}
+
+publishing {
+    publications {
+        getByName<MavenPublication>("kotlinMultiplatform") {
+            pom {
+                name.set("minipavi-core")
+                description.set("SDK pour le développement de services Minitel sur la plateforme MiniPavi en Kotlin.")
+                url.set("https://github.com/outadoc/minipavi-kotlin")
+
+                licenses {
+                    license {
+                        name.set("The Apache License, Version 2.0")
+                        url.set("http://www.apache.org/licenses/LICENSE-2.0.txt")
+                    }
+                }
+
+                developers {
+                    developer {
+                        id.set("outadoc")
+                        name.set("Baptiste Candellier")
+                        email.set("baptiste@candellier.me")
+                    }
+                }
+
+                scm {
+                    connection.set("scm:git:git://github.com/outadoc/minipavi-kotlin.git")
+                    developerConnection.set("scm:git:ssh://github.com/outadoc/minipavi-kotlin.git")
+                    url.set("https://github.com/outadoc/minipavi-kotlin")
+                }
+            }
+        }
+    }
+
+    repositories {
+        mavenLocal()
     }
 }
--- videotex/build.gradle.kts
+++ videotex/build.gradle.kts
@@ -7,6 +7,7 @@ plugins {
     alias(libs.plugins.dokka)
     alias(libs.plugins.kotlin.binaryCompatibilityValidator)
     alias(libs.plugins.spotless)
+    `maven-publish`
 }
 
 group = "fr.outadoc.minipavi"
@@ -45,5 +46,42 @@ tasks.withType<DokkaTaskPartial>().configureEach {
                 url.set(URL("https://kotlinlang.org/api/kotlinx-io/"))
             }
         }
+    }
+}
+
+publishing {
+    publications {
+        getByName<MavenPublication>("kotlinMultiplatform") {
+            pom {
+                name.set("minipavi-videotex")
+                description.set("Module de création de documents Vidéotex en Kotlin.")
+                url.set("https://github.com/outadoc/minipavi-kotlin")
+
+                licenses {
+                    license {
+                        name.set("The Apache License, Version 2.0")
+                        url.set("http://www.apache.org/licenses/LICENSE-2.0.txt")
+                    }
+                }
+
+                developers {
+                    developer {
+                        id.set("outadoc")
+                        name.set("Baptiste Candellier")
+                        email.set("baptiste@candellier.me")
+                    }
+                }
+
+                scm {
+                    connection.set("scm:git:git://github.com/outadoc/minipavi-kotlin.git")
+                    developerConnection.set("scm:git:ssh://github.com/outadoc/minipavi-kotlin.git")
+                    url.set("https://github.com/outadoc/minipavi-kotlin")
+                }
+            }
+        }
+    }
+
+    repositories {
+        mavenLocal()
     }
 }
