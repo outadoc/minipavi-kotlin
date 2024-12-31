eb0d58ad55f06844256efa61ba2cbec469a3d5f5
Baptiste Candellier <baptiste@candellier.me>
2024-12-31 19:11:05 +0100

    Try more gcloud stuff

--- sample/minitus/build.gradle.kts
+++ sample/minitus/build.gradle.kts
@@ -6,14 +6,12 @@ plugins {
     alias(libs.plugins.kotlinx.serialization)
     alias(libs.plugins.spotless)
     id("com.google.cloud.tools.appengine") version "2.8.0"
+    id("com.gradleup.shadow")
     application
 }
 
 application {
-    mainClass.set("fr.outadoc.minipavi.sample.minitus.ApplicationKt")
-    applicationDefaultJvmArgs = listOf(
-        "-Dio.ktor.development=${extra["io.ktor.development"] ?: "false"}"
-    )
+    mainClass.set("io.ktor.server.netty.EngineMain")
 }
 
 configure<AppEngineAppYamlExtension> {
