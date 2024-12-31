d271d4e38a0edb1a1a8e36131a2a9d0ac35d7a3b
Baptiste Candellier <baptiste@candellier.me>
2024-12-31 15:46:40 +0100

    Work

--- sample/minitus/src/main/kotlin/fr/outadoc/minipavi/sample/minitus/MinitusService.kt
+++ sample/minitus/src/main/kotlin/fr/outadoc/minipavi/sample/minitus/MinitusService.kt
@@ -8,15 +8,34 @@ import fr.outadoc.minipavi.videotex.VideotexBuilder
 import fr.outadoc.minipavi.videotex.buildVideotex
 import io.ktor.server.application.Application
 import io.ktor.server.application.ApplicationEnvironment
+import kotlinx.datetime.Clock
+import kotlinx.datetime.LocalDate
+import kotlinx.datetime.TimeZone
+import kotlinx.datetime.toLocalDateTime
 import kotlinx.io.readLineStrict
 import kotlinx.serialization.SerialName
 import kotlinx.serialization.Serializable
 
 @Serializable
 sealed interface MinitusState {
+
+    val date: LocalDate
+    val guesses: List<String>
+
     @Serializable
     @SerialName("intro")
-    data object IntroPage : MinitusState
+    data class Playing(
+        override val date: LocalDate,
+        override val guesses: List<String>,
+        val lastInputWasInvalid: Boolean = false,
+    ) : MinitusState
+
+    @Serializable
+    @SerialName("win")
+    data class Win(
+        override val date: LocalDate,
+        override val guesses: List<String>,
+    ) : MinitusState
 }
 
 private object Constants {
@@ -30,24 +49,90 @@ private object Constants {
 }
 
 fun Application.minitus() {
-    val words: Set<String> = readWords(environment)
+    val clock = Clock.System
+    val tz = TimeZone.of("Europe/Paris")
+
+    val dictionary: Set<String> = readWords(environment)
 
     minitelService<MinitusState>(
         path = "/",
         version = "0.1",
-        initialState = MinitusState.IntroPage,
+        initialState = MinitusState.Playing(
+            guesses = emptyList(),
+            date = clock.now().toLocalDateTime(tz).date,
+        ),
     ) { request ->
-        when (request.state) {
-            MinitusState.IntroPage -> {
-                ServiceResponse(
-                    state = MinitusState.IntroPage,
-                    content =
-                        buildVideotex {
-                            clearAll()
-                            displayLogo()
-                        },
-                )
-            }
+        val nextState = request.state.reduce(
+            date = request.state.date,
+            userInput = request.userInput.firstOrNull() ?: "",
+            dictionary = dictionary,
+        )
+
+        ServiceResponse(
+            state = nextState,
+            content =
+                buildVideotex {
+                    clearAll()
+                    displayLogo()
+
+                    showGameState(nextState)
+                },
+        )
+    }
+}
+
+private fun MinitusState.reduce(
+    date: LocalDate,
+    userInput: String,
+    dictionary: Set<String>,
+): MinitusState {
+    return when (this) {
+        is MinitusState.Playing -> reduce(date, userInput, dictionary)
+        is MinitusState.Win -> this
+    }
+}
+
+private fun MinitusState.Playing.reduce(
+    date: LocalDate,
+    userInput: String,
+    dictionary: Set<String>,
+): MinitusState {
+    val inputWord = userInput.normalize()
+    if (inputWord.isEmpty()) {
+        return this
+    }
+
+    if (inputWord !in dictionary) {
+        return copy(
+            lastInputWasInvalid = true,
+        )
+    }
+
+    val expectedWord = dictionary.pickDailyWord(date)
+
+    if (inputWord == expectedWord) {
+        return MinitusState.Win(
+            date = date,
+            guesses = guesses + inputWord,
+        )
+    }
+
+    return copy(
+        guesses = guesses + inputWord,
+        lastInputWasInvalid = false,
+    )
+}
+
+private fun VideotexBuilder.showGameState(state: MinitusState) {
+    if (state is MinitusState.Playing && state.lastInputWasInvalid) {
+        withTextColor(Color.Red) {
+            appendLine("Désolé, ce mot n'est pas dans mon dictionnaire.")
+        }
+    }
+
+    if (state is MinitusState.Win) {
+        withTextColor(Color.Green) {
+            appendLine("Bravo, vous avez trouvé le mot du jour !")
         }
     }
 }
